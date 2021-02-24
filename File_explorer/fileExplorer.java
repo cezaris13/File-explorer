768098d2abc287cf88ce581db4b845846fdeb766
cezaris13 <pijus.cezar13@gmail.com>
2021-02-24 20:57:07 +0200

    test with magit

--- File_explorer/fileExplorer.java
+++ File_explorer/fileExplorer.java
@@ -6,28 +6,34 @@ import java.io.IOException;
 public class fileExplorer {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-    public static void recursiveFiles(String dirpath,int count){
-    String space = new String(new char[count]).replace('\0', '\t');
-    File f = new File(dirpath, ""); 
+    public static void recursiveFiles(String dirpath,int count,String ex){
+        String space = new String(new char[count]).replace('\0', '\t');
+        File f = new File(dirpath); 
         if(f.exists()){
-            System.out.println(ANSI_BLUE+dirpath+ANSI_RESET);
-            String list[]= f.list();
+            String list[]=f.list();
             int n=list.length;
             for(int i=0;i<n;i++){
-                System.out.print(space+list[i]);
-                File f1=new File(dirpath+"/"+list[i]);
+                File f1= new File(dirpath+"/"+list[i]);
                 if(f1.isFile()){
-                    System.out.println(ANSI_RED+":this is file"+ANSI_RESET);
+                    String extension = "";
+                    int j = f1.getName().lastIndexOf('.');
+                    if (j >= 0) {
+                        extension = f1.getName().substring(j+1);//something.txt -> txt
+                    }
+                    if(extension.equals(ex) || ex.equals("")){
+                        System.out.print(space+list[i]);
+                        System.out.println(ANSI_RED+":this is a file"+ANSI_RESET);
+                    }                   
                 }
                 if(f1.isDirectory()){
-                    System.out.println(ANSI_PURPLE+":this is directory"+ANSI_RESET);
-                    recursiveFiles(dirpath+"/"+list[i],count+1);
+                    System.out.print(space+dirpath+"/"+list[i]);
+                    System.out.println(ANSI_PURPLE+":this is a directory"+ANSI_RESET);
+                    recursiveFiles(dirpath+"/"+list[i],count+1,ex);
                 }
             }
             if(n==0){
-                System.out.println("no entries in this directory");
+                System.out.println(space+"no entries in this directory");
             }
         }
         else{
@@ -36,9 +42,10 @@ public class fileExplorer {
     }    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-  
         System.out.println("Enter dirpath:"); 
-        String dirpath=br.readLine(); 
-        recursiveFiles(dirpath,0);
+        String dirpath = br.readLine(); 
+        System.out.println("Enter filetype:"); 
+        String ex = br.readLine();
+        recursiveFiles(dirpath,0,ex);
     }
 }
