1d32fa76e3b17a3329eccad67afee334328b6671
cezaris13 <pijus.cezar13@gmail.com>
2021-02-27 13:59:50 +0200

    file/dir functions + simple gui

--- Normal_file_explorer/fileApp.java
+++ Normal_file_explorer/fileApp.java
@@ -38,52 +38,68 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import javax.swing.*;
 
 public class fileApp {
-  public static void main(String[] args) {
-    JPanel panel = new JPanel() {
-      @Override
-      public Dimension getPreferredSize() {
-        return new Dimension(200, 200);
-      }
-    };
-    JButton[] bb = new JButton[10];
+    static int n=1;
+    static String list[]=new String[100];
+    static JButton[] buttons=new JButton[100];
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
 
-    
-    // JButton b=new JButton(new ImageIcon("/home/pijus/Desktop/Java/Normal_file_explorer/folder.png"));
-    // b.setBounds(50,100,95,30);
-    // b.addActionListener(new ActionListener(){
-    //                 public void actionPerformed(ActionEvent e){
-    //                         // b.setBounds(900,100,95,30);
-    //                 }
-    //         });
-   
-    JTabbedPane tabbedPane = new JTabbedPane();
-    tabbedPane.addTab("test", panel);
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLayout(new BorderLayout());
-    frame.add(tabbedPane);
-    ImageIcon folder = new ImageIcon("/home/pijus/Desktop/Java/Normal_file_explorer/folder.png");
-    int width= folder.getImage().getWidth(null);
-    int height= folder.getImage().getHeight(null);
-    for(int i=0;i<10;i++){
-      bb[i]=new JButton(new ImageIcon("/home/pijus/Desktop/Java/Normal_file_explorer/folder.png"));
-      frame.add(bb[i]);
+        ImageIcon fileIcon = new ImageIcon("/home/pijus/Desktop/Java/Normal_file_explorer/file.png");
+        ImageIcon folderIcon = new ImageIcon("/home/pijus/Desktop/Java/Normal_file_explorer/folder.png");
+        int folWidth  = folderIcon.getImage().getWidth(null);
+        int folHeight = folderIcon.getImage().getHeight(null);
+        int filWidth  = fileIcon.getImage().getWidth(null);
+        int filHeight = fileIcon.getImage().getHeight(null);
+        // File f = new File(args[0]);
+        String dirpath= "/home/pijus/Desktop";
+        File f = new File(dirpath);
+        if(f.exists()){
+            list=f.list();
+            n=list.length;
+            buttons = new JButton[n];
+            for(int i=0;i<n;i++){
+                File f1= new File(dirpath+"/"+list[i]);
+                if(f1.isFile()){
+                    buttons[i]=new JButton(fileIcon);
+                    frame.add(buttons[i]);
+                }
+                else if(f1.isDirectory()){
+                    buttons[i]=new JButton(folderIcon);
+                    frame.add(buttons[i]);
+                }
+            }
+        }
+        frame.addComponentListener(new ComponentAdapter() {
+                public void componentResized(ComponentEvent componentEvent) {
+                    int space=10;
+                    // b.setBounds(componentEvent.getComponent().getSize().width/2,componentEvent.getComponent().getSize().height/2,95,30);
+                    int x = componentEvent.getComponent().getSize().width/space;//tarpo ir aukscio konstantas kazkokias reik
+                    int y = componentEvent.getComponent().getSize().height/space;
+                    int countx=0;
+                    int county=0;
+                    for(int i=0;i<n;i++){
+                        if(x*countx>x*(space-1)){
+                            county++;
+                            countx=0;
+                        }
+                        File f1= new File(dirpath+"/"+list[i]);
+                        if(f1.isFile()){
+                            buttons[i].setBounds(countx*x,county*y,filWidth,filHeight);
+                        }
+                        else if(f1.isDirectory()){
+                            buttons[i].setBounds(countx*x,county*y,folWidth,folHeight);
+                        }
+                        countx++;
+                    }
+                }
+            });
+        frame.setSize(500,500);
+        frame.setVisible(true);
     }
-    // frame.add(b);
-    // frame.pack();
-    frame.setSize(500,500);
-    frame.setVisible(true);
-    frame.addComponentListener(new ComponentAdapter() {
-      public void componentResized(ComponentEvent componentEvent) {
-          // b.setBounds(componentEvent.getComponent().getSize().width/2,componentEvent.getComponent().getSize().height/2,95,30);
-          int x = componentEvent.getComponent().getSize().width/10;
-          for(int i=1;i<10;i++){
-            bb[i].setBounds(i*x,100,width,height);
-          }
-      }
-  });
-  }
 }
---
+++ Normal_file_explorer/file_management.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class file_management{
+
+    static void createFile(String name,String location){
+        try {
+            File file = new File(location+"/"+name);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    static void deleteFile(String name,String location){
+        File file = new File(location+"/"+name);
+        if (file.delete()) {
+            System.out.println("Deleted the file: " + file.getName());
+        }
+        else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
+    static void renameFile(String name,String newName, String location){
+        File oldFile = new File(location+"/"+name);
+        File newFile = new File(location+"/"+newName);
+        if (newFile.exists()){
+            System.out.println("file exists");
+            return;
+        }
+        boolean success = oldFile.renameTo(newFile);
+        if (!success) {
+            System.out.println("failed to rename file");
+        }
+    }
+    static void createDirectory(String name,String location){
+        File dir = new File(location+"/"+name);
+        boolean create=dir.mkdir();
+        if(create){
+            System.out.println("created directory");
+        }
+        else{
+            System.out.println("failed to create directory");
+        }
+    }
+    static void deleteDirectory(String name,String location) {
+        File dir = new File(location+"/"+name);
+        if(dir.length()>0){
+            // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("There are files in this directory. Do you want to delete this directory? y/n");
+            // String ans = br.readLine();
+            // if(ans.equals("y")){
+
+            // }
+        }
+        boolean del=dir.delete();
+        if(del){
+            System.out.println("deleted directory");
+        }
+        else{
+            System.out.println("failed to create directory");
+        }
+    }
+    static void renameDirectory(String name,String newName,String location){
+        File oldDir = new File(location+"/"+name);
+        File newDir = new File(location+"/"+newName);
+        if (newDir.exists()){
+            System.out.println("directory exists");
+            return;
+        }
+        boolean success = oldDir.renameTo(newDir);
+        if (!success) {
+            System.out.println("failed to rename directory");
+        }
+    }
+    //copy file && copy directory+ delete directory if something is in there
+
+    // public static void main(String args[]){
+
+    //     System.out.println("hellp");
+    //     // createFile("kazkas.txt","/home/pijus/Desktop/empty");
+    //     // createDirectory("kazkas.txt","/home/pijus/Desktop/empty");
+    //     // deleteFile("kazkas.txt","/home/pijus/Desktop/empty");
+    //     deleteDirectory("temp","/home/pijus/Desktop/empty");
+    //     // renameFile("kazkas.txt","kazkas2.txt","/home/pijus/Desktop/empty");
+    //     // renameDirectory("naujas_folderis","temp","/home/pijus/Desktop/empty");
+    // }
+}
